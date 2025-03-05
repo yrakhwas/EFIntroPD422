135f36d2dcc2746cf337a76e7e95ab3b8b12bc45
yrakhwas <yrakhwas@gmail.com>
2025-03-05 16:51:36 +0200

    Add project files.

---
+++ AirLinesDbContext.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace EFIntroPD422
+{
+    class AirLinesDbContext: DbContext
+    {
+        public AirLinesDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = SuperAirLinesDB; Integrated Security = True; Connect Timeout = 5; Encrypt = False; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False");
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Airplane> Airplanes { get; set; }
+    }
+    //Clients
+    //Flights
+    //Aiplanes
+    class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthdate { get; set; }
+        public ICollection<Flight> Flights { get; set; }
+    }
+    class Flight
+    {
+        public int Id { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+
+        public string DepartureCity { get; set; }
+        public string ArrivalCity { get; set; }
+        public Airplane Airplain { get; set; }
+        public ICollection<Client> Clients { get; set; }
+
+    }
+    class Airplane
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public int MaxPassengers { get; set; }
+        public ICollection<Flight> Flights { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EFIntroPD422
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AirLinesDbContext context = new AirLinesDbContext();
+            context.Clients.Add(new Client
+            {
+                Name = "Ivan",
+                Email = "ivan@gmail.com",
+                Birthdate = new DateTime(1990, 1, 1),
+            });
+            context.SaveChanges();
+
+
+            foreach (var client in context.Clients)
+            {
+                Console.WriteLine($"Client : {client.Name} - {client.Email} - {client.Birthdate.ToShortDateString()}");
+            }
+        }
+    }
+}
