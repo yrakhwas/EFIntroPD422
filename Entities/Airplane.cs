62c18deda4cde113735875e39e4e876063a73ba8
yrakhwas <yrakhwas@gmail.com>
2025-03-12 15:20:30 +0200

    Before add FluentAPI

--- Entities/Airplane.cs
+++ Entities/Airplane.cs
@@ -1,9 +1,11 @@
-﻿namespace EFIntroPD422.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFIntroPD422.Entities
 {
     class Airplane
     {
         public int Id { get; set; }
-
+        [Required]
         public string Model { get; set; }
         public int MaxPassengers { get; set; }
         public ICollection<Flight> Flights { get; set; }
